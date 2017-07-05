deede333c727ad279f4388c9827626aa2306e9ba
nararacds <nararacds@gmail.com>
2017-07-05 19:21:24 +0900

    complete chapter 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (Rigidbody))]
 public class PlayerController : MonoBehaviour {
 
+	Vector3 velocity;
 	Rigidbody myRigidBody;
 
 	void Start () {
-	
-	}
-	
-	public void Move(Vector3 velocity){
+		myRigidBody = GetComponent<Rigidbody> ();
 
 	}
+	
+	public void Move(Vector3 _velocity){
+		velocity = _velocity;
+	}
+
+	public void LookAt(Vector3 LookPoint){
+		Vector3 heightCorrectedPoint = new Vector3 (LookPoint.x, transform.position.y, LookPoint.z);
+		transform.LookAt (heightCorrectedPoint);
+	}
+
+	public void FixedUpdate(){
+		myRigidBody.MovePosition (myRigidBody.position + velocity * Time.fixedDeltaTime);
+	}
+
 }
