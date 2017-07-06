58bf56157c8c38275043f285a0b24d970362fa80
nararacds <nararacds@gmail.com>
2017-07-06 15:48:13 +0900

    some

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController : MonoBehaviour {
+
+	public Transform weaponHold;
+	public Gun startingGun;
+	Gun equippedGun;
+
+	void Start() {
+		if (startingGun != null) {
+			EquipGun (startingGun);
+		}
+	}
+
+	public void EquipGun(Gun gunToEquip) {
+		if (equippedGun != null) {
+			Destroy (equippedGun.gameObject);
+		}
+		equippedGun = Instantiate (gunToEquip, weaponHold.position, weaponHold.rotation) as Gun;
+		equippedGun.transform.parent = weaponHold;
+
+	}
+}
