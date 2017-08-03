f8f5168396734b72867d2f631e22e1515d76adee
nararacds <nararacds@gmail.com>
2017-08-03 14:09:30 +0900

    Episode12

--- Assets/Editor/MapEditor.cs
+++ Assets/Editor/MapEditor.cs
@@ -7,11 +7,22 @@ public class MapEditor : Editor {
 
 	public override void OnInspectorGUI ()
 	{
-		base.OnInspectorGUI ();
-
 		MapGenerator map = target as MapGenerator;
 
-		map.GenerateMap ();
+		if (DrawDefaultInspector ()) {
+			map.GenerateMap ();
+		}
+
+		if (GUILayout.Button("Generate Map")) {
+			map.GenerateMap ();
+		}
+
+
+		//base.OnInspectorGUI ();
+
+		//MapGenerator map = target as MapGenerator;
+
+		//map.GenerateMap ();
 
 	}
 }
