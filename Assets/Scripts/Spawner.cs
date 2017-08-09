ebd5c6a6ad77ca03594db8a2ed7b8782fd4b4e22
nararacds <nararacds@gmail.com>
2017-08-09 21:46:09 +0900

    Episode18

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour {
 
+	public bool devMode;
+
 	public Wave[] waves;
 	public Enemy enemy;
 
@@ -51,14 +53,24 @@ public class Spawner : MonoBehaviour {
 
 			}
 
-			if (enemiesRemainingToSpawn > 0 && Time.time > nextSpawnTime) {
+			if ((enemiesRemainingToSpawn > 0 || currentWave.infinite) && Time.time > nextSpawnTime) {
 				enemiesRemainingToSpawn--;
 				nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
 
-				StartCoroutine (SpawnEnemy ());
+				StartCoroutine ("SpawnEnemy");
 			}
 		}
 
+		if (devMode) {
+			if (Input.GetKeyDown (KeyCode.Return)) {
+				StopCoroutine ("SpawnEnemy");
+				foreach (Enemy enemy in FindObjectsOfType<Enemy>()) {
+					GameObject.Destroy (enemy.gameObject);
+				}
+
+				NextWave ();
+			}
+		}
 	}
 
 	IEnumerator SpawnEnemy() {
@@ -85,6 +97,7 @@ public class Spawner : MonoBehaviour {
 
 		Enemy spawnedEnemy = Instantiate (enemy, spawnTile.position + Vector3.up, Quaternion.identity) as Enemy;
 		spawnedEnemy.OnDeath += OnEnemyDeath;
+		spawnedEnemy.SetCharacteristics (currentWave.moveSpeed, currentWave.hitsToKillPlayer, currentWave.enemyHealth, currentWave.skinColour);
 	}
 
 	void OnPlayerDeath() {
@@ -125,10 +138,14 @@ public class Spawner : MonoBehaviour {
 
 	[System.Serializable]
 	public class Wave {
+		public bool infinite;
 		public int enemyCount;
 		public float timeBetweenSpawns;
 
-
+		public float moveSpeed;
+		public int hitsToKillPlayer;
+		public float enemyHealth;
+		public Color skinColour;
 	}
 
 }
